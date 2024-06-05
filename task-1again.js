a96a72be0baa9dcbcef12c4d77d8caa475c2b382
ahmadqadomi-1 <qadomia95@gmail.com>
2024-06-05 05:58:43 +0300

    task 4-6

--- task-1again.js
+++ task-1again.js
@@ -3,7 +3,7 @@ let x = 250 ;
 create variable Calculate the value of zakat for x ,If you know the percentage of zakat = 2.5 %; 
 */
 let zakat = 250;
-let zakatPercent = .025;
+let zakatPercent = .025 ;
 zakatValue = zakat * zakatPercent;
 console.log("The value of zakat for $" + zakat + " is $" + zakatValue);
 
@@ -15,20 +15,21 @@ Correct the syntax error
 ["Str" "alex","moh"
 'the','fox' 'over' lazy, 'dog',  ]
 */
-[ 1 , 7 , 9 , 45 ]
-
-[ "Str" , "alex" ,"moh" ]
-
-[ 'the' ,'fox' , 'over' , 'lazy' , 'dog'  ]
+let fr= [ 1, 7 , 9 , 4  , 5 ];
+let sc= [ "Str" , "alex" , "moh"];
+let th= [ 'the' ,'fox' , 'over' , 'lazy' , 'dog'  ];
+console.log (fr);
+console.log (sc);
+console.log (th);
 
 /*
 2
 What the index of "Banana","Tomato"
 var fruits=["Tomato","Banana","Watermelon"]
 */
-var fruits=["Tomato","Banana","Watermelon"]
-fruits[0]="Tomato";
-fruits[1]="Banana";
+var fruits=["Tomato","Banana","Watermelon"];
+console.log(fruits.indexOf ("Tomato"));
+console.log(fruits.indexOf ("Banana"));
 
 /*
 3
@@ -37,9 +38,10 @@ Create an array represents your:
 2- Favorite Sport (3)
 3- Favorite Movie (2)
 */
-let favFood = [ "Mansaf" , "Burger" ];
-let favSport = [ "Football" , "Sleeping" , "Eating" ];
-let favMovie = [ "The Meg" , "çukur" ];
+let foodd=['food1' , 'food2'];
+let sportt=[ 'sp1' , 'sp2' , 'sp3'];
+let moviee=[ 'mov1' , 'mov2'];
+console.log( moviee , sportt , foodd);
 
 /*
 4
@@ -47,42 +49,33 @@ Create a Variable to return the first element in an array
 Ex: firstOfArray([1,4,5]) => 1
 Ex: firstOfArray(["t","u","g","x"]) => "t"
 */
-let array1= [1,4,5];
-console.log(array1[0]);
-let array2= ["t","u","g","x"];
-console.log(array2[0]);
+let ar= [1,4,5];
+console.log(ar[0]);
+let ar1= ["t","u","g","x"];
+console.log(ar1 [0]);
 
 /*
-5
-Create a Variable to return the lastOfArray element in an array 
-
 Ex: lastOfArray([1,4,5]) => 5
 Ex: lastOfArray(["t","u","g","x"]) => "x"
 */
-let arr1= [1,4,5];
-console.log(array1[-1]);
-let arr2= ["t","u","g","x"];
-console.log(array2[-1]);
+let ar01= [1,4,5];
+console.log(ar01[ar01.length -1]);
+let ar02= ["t","u","g","x"];
+console.log(ar02[ar02.length -1]);
 
 /*
 6
 Using console make this array to be like this one (push, unshift, shift, pop)
-
 var array = [0,5,7,9]
 => [1,3,4,6,8,9,10]
 */
-let num= [0,5,7,9];
-num.
-num.shift();
-num.shift();
-num.shift();
-num.shift();
-num.unshift(1, 3, 4, 6, 8, 10);
-console.log(num);
-//يمكن فعل ذلك من خلال حذف العناصر عن طريق إستخدام
-//pop
-// وإضافة العناصر عن طريق إستخدام
-//push
+let ar03=  [0,5,7,9];
+ar03.pop();
+ar03.pop();
+ar03.pop();
+ar03.pop();
+ar03.push(1,3,4,6,8,9,10);
+console.log(ar03);
 
 /*
 7
@@ -90,15 +83,14 @@ Using the console try to figure out what the thing thats (push, unshift, shift, 
 
 var array2 = [5,9,-7,3.5]
 */
-//rtl?
-var array02 = [ 5 , 9 , -7 , 3.5 ];
-array02.shift();
-array02.shift();
-array02.unshift(3.5);
-array02.pop();
-array02.push(9);
-array02.push(5);
-console.log(array02);
+let ar04= [5,9,-7,3.5];
+ar04.shift();
+ar04.shift();
+ar04.shift();
+ar04.push(-7);
+ar04.push(9);
+ar04.push(5);
+console.log(ar04);
 
 /*
 8.
@@ -106,7 +98,7 @@ Write a JavaScript program to sort the items of an array.
 Sample array : var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
 Sample Output : -4,-3,1,2,3,5,6,7,8
 */
-var arr01= [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
-arr01.sort();
-console.log(arr01);
-
+var ar05 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ]
+console.log (ar05.sort (function (a,b)
+    {return a - b
+}))
---
+++ task-2.js
@@ -0,0 +1,100 @@
+/* 1. Write a JavaScript program that accept two integers and
+ * display the larger.Go to the editor */
+/******* Start Your Code *********/
+let num = numberLarger(3,5);
+function numberLarger ( a ,b ){
+    if(a>b){
+        return a ;
+    }else {
+        return b
+    }
+}
+console.log(num);
+ /******* End Your Code ********* */
+
+/* 2. Write a JavaScript conditional statement to find the sign
+of product of three numbers. Display an alert box with the 
+specified sign. Go to the editor 
+Sample numbers : 3, -7, 2 
+Output : The sign is - */
+/******* Start Your Code *********/
+function sign(a, b, c) {
+    let array01 = a * b * c;
+    if (array01 > 0) {
+        return "+";
+    } else if (array01 < 0) {
+        return "-";
+    } else {
+        return "0";
+    }
+}
+let array1 = sign(3, -7, 2);
+console.log("The sign is", array1);
+ /******* End Your Code ********* */
+
+/* 3. Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the result. Go to the editor 
+Sample numbers : 0, -1, 4 
+Output : 4, 0, -1 */
+
+
+/******* Start Your Code *********/
+
+let arr01 = 0;
+let arr02 = -1;
+let arr03 = 4;
+
+if (arr01 >= arr02 && arr01 >= arr03) {
+    if (arr02 >= arr03) {
+        alert(arr01 + ", " + arr02 + ", " + arr03);
+    } else {
+        alert(arr01 + ", " + arr03 + ", " + arr02);
+    }
+} else if (arr02 >= arr01 && arr02 >= arr03) {
+    if (arr01 >= arr03) {
+        alert(arr02 + ", " + arr01 + ", " + arr03);
+    } else {
+        alert(arr02 + ", " + arr03 + ", " + arr01);
+    }
+} else {
+    if (arr01 >= arr02) {
+        alert(arr03 + ", " + arr01 + ", " + arr02);
+    } else {
+        alert(arr03 + ", " + arr02 + ", " + arr01);
+    }
+}
+ /******* End Your Code ********* */
+
+/*4. Write a JavaScript conditional statement to find the largest of five numbers. 
+Display an alert box to show the result. Go to the editor 
+Sample numbers : -5, -2, -6, 0, -1 
+Output : 0 */
+/******* Start Your Code *********/
+let arr11 = -5;
+let arr12 = -2;
+let arr13 = -6;
+let arr14 = 0;
+let arr15 = -1;
+let largestNumber = arr11;
+if (arr12 > largestNumber) {
+    largestNumber = arr12;
+}
+if (arr13 > largestNumber) {
+    largestNumber = arr13;
+}
+if (arr14 > largestNumber) {
+    largestNumber = arr14;
+}
+if (arr15 > largestNumber) {
+    largestNumber = arr15;
+}
+alert("The largest number is: " + largestNumber);
+ /******* End Your Code ********* */
+
+/* 5.Write the if statement to display "Hello World" if x is greater than y, otherwise alert "Goodbye".*/
+/******* Start Your Code *********/
+if (x > y) {
+    console.log("Hello World");
+} else {
+    alert("Goodbye");
+}
+/******* End Your Code ********* */
